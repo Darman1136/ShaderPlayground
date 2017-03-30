644d78b0f23567673bcd81356ba4ce2d9d696408
Florian Dunz <X-TremeGamer@web.de>
2017-03-30 18:26:27 +0200

    WaterRipple: added water ripple effect

---
+++ ShaderPlayground/Assets/Code/WaterRipple/CWaterRippleCollisionScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CWaterRippleCollisionScript : MonoBehaviour {
+    private readonly string S_OFFSET = "_Offset";
+    private readonly string S_WAVE_AMPLITUDE = "_WaveAmplitude";
+    private readonly string S_IMPACT = "_Impact";
+    private readonly string S_DRAW_DISTANCE = "_DrawDistance";
+
+    private Renderer r;
+    private Mesh mesh;
+
+    private float[] faWaveAmplitude = new float[8];
+    private float[] faDrawDistance = new float[8];
+    private Vector4[] vec4aOffset = new Vector4[8];
+    private Vector4[] vec4aImpact = new Vector4[8];
+
+    private int iWaveNumber = 0;
+
+    public float fMagnitudeDivider = 0.1f;
+
+    void Start() {
+        r = GetComponent<Renderer>();
+        mesh = GetComponent<MeshFilter>().mesh;
+
+    }
+
+    void Update() {
+        for (int index = 0; index < faWaveAmplitude.Length; index++) {
+            if (faWaveAmplitude[index] > 0.01f) {
+                faWaveAmplitude[index] *= .98f ;
+                faDrawDistance[index] += .05f;
+            } else {
+                faWaveAmplitude[index] = 0f;
+                faDrawDistance[index] = 1f;
+            }
+        }
+
+        /*
+        mTime += Time.deltaTime * iSpeed;
+
+        mMaterial.SetFloat("_Offset", Mathf.Repeat(mTime, 1.0f));
+        */
+
+        UpdateShaderFloats();
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.rigidbody) {
+            if (collision.contacts.Length > 0) {
+                Vector3 collisionPoint = collision.contacts[0].point;
+                vec4aOffset[iWaveNumber] = transform.InverseTransformPoint(collisionPoint) * -2f;
+                vec4aImpact[iWaveNumber] = transform.InverseTransformPoint(collisionPoint);
+                faWaveAmplitude[iWaveNumber] = collision.rigidbody.velocity.magnitude * fMagnitudeDivider;
+                faDrawDistance[iWaveNumber] = 1f;
+
+                iWaveNumber++;
+                if (iWaveNumber > 7) {
+                    iWaveNumber = 0;
+                }
+            }
+        }
+    }
+
+    private void UpdateShaderFloats() {
+        r.material.SetFloatArray(S_WAVE_AMPLITUDE, faWaveAmplitude);
+        r.material.SetFloatArray(S_DRAW_DISTANCE, faDrawDistance);
+        r.material.SetVectorArray(S_OFFSET, vec4aOffset);
+        r.material.SetVectorArray(S_IMPACT, vec4aImpact);
+    }
+}
---
+++ ShaderPlayground/Assets/Shaders/WaterRipple/WaterRippleShader.shader
@@ -0,0 +1,66 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Custom/WaterRippleShader" {
+	Properties{
+		_Color("Color", Color) = (1,1,1,1)
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness("Smoothness", Range(0,1)) = 0.5
+		_Metallic("Metallic", Range(0,1)) = 0.0
+		_Scale("Scale", float) = 1
+		_Speed("Speed", float) = 1
+		_Frequency("Frequency", float) = 1
+	}
+		SubShader{
+			Tags { "RenderType" = "Opaque" }
+			LOD 200
+
+			CGPROGRAM
+			// Physically based Standard lighting model, and enable shadows on all light types
+			#pragma surface surf Lambert vertex:vert
+
+			// Use shader model 3.0 target, to get nicer looking lighting
+			#pragma target 4.0
+
+			sampler2D _MainTex;
+			float _Scale, _Speed, _Frequency;
+
+			struct Input {
+				float2 uv_MainTex;
+				float3 normalValue;
+			};
+
+			uniform float _WaveAmplitude[8];
+			uniform half4 _Offset[8];
+			uniform float _DrawDistance[8];
+			uniform half4 _Impact[8];
+
+			half _Glossiness;
+			half _Metallic;
+			half4 _Color;
+
+			void vert(inout appdata_full v, out Input o) {
+				UNITY_INITIALIZE_OUTPUT(Input, o);
+				half ripple = ((v.vertex.x * v.vertex.x) + (v.vertex.z * v.vertex.z));
+				float3 worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;
+
+				for (int i = 0; i < 8; i++) {
+					half value = _Scale * sin(_Time.w * _Speed * _Frequency + ripple + ((v.vertex.x * _Offset[i].x) + (v.vertex.z * _Offset[i].z)));
+					if (distance(v.vertex, _Impact[i]) < _DrawDistance[i]) {
+						o.normalValue += (value  * _WaveAmplitude[i]);
+						v.vertex.y += (value * _WaveAmplitude[i]);
+					}
+				}
+			}
+
+			void surf(Input IN, inout SurfaceOutput o) {
+				// Albedo comes from a texture tinted by color
+				half4 c = tex2D(_MainTex, IN.uv_MainTex);
+				o.Albedo = _Color.rgb;
+				o.Alpha = c.a;
+				o.Normal.y = IN.normalValue;
+			}
+
+ENDCG
+		}
+			FallBack "Diffuse"
+}
